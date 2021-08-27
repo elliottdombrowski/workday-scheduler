81e4ca111f2e6e23c75d06505a22c88569edac00
elliottdombrowski <elliottroyal@me.com>
2021-08-27 00:39:03 -0500

    Each icon has an individual button click based on index. Dynamically updates row classes with moment.js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,16 +28,16 @@ for (i = 0; i < 9; i++) {
         className = "future";
     }
 
+    const row = "tableRow" + i;
     container.append(
             '<tr class="row">'+
             '<th class="hour">' + times[i] + '</th>'+
             '<td id="colorCoded1" class="' + className + '" contenteditable="true"></td>'+
-            '<td class="saveBtn">'+
+            '<td class="saveBtn" id="' + row + '">'+
             '<span><i class="fas fa-save"></i></span>'+
             '</td>'+
           '</tr>');
 
-    const row = "tableRow" + i;
     $("#tableRow" + i).click(function() {
         alert("hi i'm " + row);
     });
