fb496d8c3b1d8940182a2e7676f60c508a3189ff
elliottdombrowski <elliottroyal@me.com>
2021-08-28 15:02:19 -0500

    implemented local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,5 @@
 var date = moment();
 var today = moment().format("MMM Do, YYYY, hh A");
-// $("#currentDay").text(date.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-
-function setTime() {
-    setInterval(function() {
-        today = moment().format("MMM Do, YYYY, hh A");
-        $("#currentDay").text(today);
-    }, 1000);
-  }
-
-setTime();
 
 var container = $(".container");
 var button = $(".saveBtn");
@@ -26,14 +16,22 @@ var times = [
     "4pm",
     "5pm"
 ]
-    
-getStorage();
 
+//Displays moment.js time format on the page, refreshes every second
+function setTime() {
+    setInterval(function() {
+        today = moment().format("MMM Do, YYYY, hh A");
+        $("#currentDay").text(today);
+    }, 1000);
+}
+
+//Evaluates index position for each table row against moment.js time.
 for (i = 0; i < 9; i++) {
     var className = "present";
     var currentHour = parseInt(date.format("H"));
     var hour = currentHour - 9;
 
+//Will update table row with appropriate class to change color
     if (i < hour) {
         className = "past";
     } else if (i === hour) {
@@ -43,38 +41,50 @@ for (i = 0; i < 9; i++) {
     }
 
     const row = "tableRow" + i;
+    const textSave = "text-input" + i;
+    var timeslot = times[i];
 
+    //On page load, appends a table row for each working hour (9-5p)
     container.append(
             '<tr class="row">'+
             '<th class="hour">' + times[i] + '</th>'+
-            '<td class="' + className + ' text-save" contenteditable="true"></td>'+
+            '<td class="' + className + ' text-save" id="' + textSave + '" contenteditable="true"></td>'+
             '<td class="saveBtn">'+
             '<span><i class="fas fa-save" id="' + row + '"></i></span>'+
             '</td>'+
             '</tr>');
 
-    $("#tableRow" + i).click(function() {
-        alert("hi i'm " + row);
-        setStorage();
-    });
+    $("#tableRow" + i).click(storeAllRows);
 }
 
-//
-function setStorage() {
-    $('.text-save').on("click",function(event){
-        event.preventDefault();
-        var timeblock = $(this).attr('id').split('-')[0]
-        var userplan = $(`#${timeblock}-plan`).val()
-        localStorage.setItem(timeblock, userplan);
-        console.log(timeblock,userplan);
-    })
-}
-// function setStorage() {
-//     localStorage.setItem("text", textArea.value);
-//     console.log(localStorage);
-// }
 
+function storeAllRows() {
+    for (var i = 0; i < 9; i++) {
+        const saveKey = times[i];
+        const textSave = "#text-input" + i;
+        var saveValue = $(textSave).html();
+        // alert(saveKey + " set to " + saveValue);
+        console.log("looking at: " + textSave);
+        console.log(saveKey + " set to " + saveValue);
+        localStorage.setItem(saveKey, saveValue);
+    }
+}
+
+
+//Pull Local Storage 
 function getStorage() {
-    var lastRender = localStorage.getItem("text");
-    textArea.textContent = lastRender;
+    for (var i = 0; i < 9; i++) {
+        const saveKey = times[i];
+        const textSave = "#text-input" + i;
+        var setValue = localStorage.getItem(saveKey);
+
+        console.log("pulling " + saveKey);
+
+        $(textSave).html(setValue);
+    }
 }
+
+//Calls setTime on page load to display moment.js clock
+setTime();
+//Calls Local Storage to be pulled on page load
+getStorage();
